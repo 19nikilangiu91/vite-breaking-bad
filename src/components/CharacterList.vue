67380b8b5965b0733de4355074b7b23b284c2888
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-14 19:00:19 +0100

    Import SingleCharacter in script + comp + temp

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,9 +1,15 @@
 <script>
+// Importo il componente "SingleCharacter"
+import SingleCharacter from './SingleCharacter.vue'
+
 // Importiamo lo "store"
 import { store } from '../store.js';
 
 export default {
     name: "CharacterList",
+    components: {
+        SingleCharacter,
+    },
     data() {
         return {
             store,
@@ -17,13 +23,7 @@ export default {
 <template>
     <div class="found"></div>
     <section class="container">
-        <!-- Template di card singola -->
-        <div class="character">
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="image">
-            <h4 class="name">nome</h4>
-            <div class="status">stato</div>
-            <div class="species">specie</div>
-        </div>
+        <SingleCharacter />
     </section>
 </template>
 
@@ -42,28 +42,5 @@ export default {
     width: 90%;
     height: 500px;
     margin: 0px auto;
-
-    .character {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        width: 120px;
-        height: 200px;
-        margin: 5px;
-        background-color: red;
-
-        img {
-            width: 100px;
-            height: 100px;
-            align-self: center;
-        }
-
-        .name,
-        .status,
-        .species {
-            margin-top: 5px;
-            align-self: center;
-        }
-    }
 }
 </style>
