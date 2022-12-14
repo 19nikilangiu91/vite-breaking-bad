4ed8d1eb6675e31dd223292dddc05fb1167fe2e0
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-14 17:14:04 +0100

    Create AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+
+</script>
+
+<template>
+    <header>
+        <h1>Rick and morty</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: white;
+}
+</style>
