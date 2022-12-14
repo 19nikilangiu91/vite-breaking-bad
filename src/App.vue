c6b834d95964965895320f632a8129d6ac662f60
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-14 17:53:52 +0100

    Added Template and General and Variables Style

--- src/App.vue
+++ src/App.vue
@@ -19,15 +19,25 @@ export default {
 <template>
 
   <body>
-    <AppHeader />
+    <!-- Vado a prendere da "store" il "titolo" -->
+    <AppHeader :msg="store.titolo" />
+    <main>
+
+    </main>
   </body>
 </template>
 
 <style lang="scss" >
+@use './style/general.scss' as*;
+@use './style/partials/variables' as*;
+
 body {
   width: 100%;
   height: 100vh;
   background-color: #2e3a46;
+}
 
+h1 {
+  color: $primary;
 }
 </style>
