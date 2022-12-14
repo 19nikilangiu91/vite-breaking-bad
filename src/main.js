36fea6f7232db6988f70d6383014a463f9492ffe
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-14 21:19:43 +0100

    Added FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faAngleDown)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
