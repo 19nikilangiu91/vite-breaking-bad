c0bec73f8b3bd904dc9ed8a8f1b7f0890e0bb994
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-14 17:55:40 +0100

    Upgrade Style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,20 @@
 <script>
 export default {
     name: "AppHeader",
+    // Creo la props per inviare il "msg" da "App.vue" e vado a stamparlo nell' "h1".
+    props: ["msg"]
 }
 
 </script>
 
 <template>
     <header>
-        <h1>Rick and morty</h1>
+        <h1>{{ msg }}</h1>
     </header>
 </template>
 
 <style lang="scss" scoped>
 h1 {
-    color: white;
+    margin: 20px 0 0 20px;
 }
 </style>
