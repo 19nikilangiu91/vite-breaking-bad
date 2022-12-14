190adda5f33dcec390e553ddacf01b52dee04d97
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-14 16:24:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss" >
+@use './style/general.scss' as*;
+</style>
